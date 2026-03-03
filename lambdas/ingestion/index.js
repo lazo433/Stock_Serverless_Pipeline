5464b8a635e3abcbeeecab0a21a2b8e24ccda1c2
Henry <lazoo310@hotmail.com>
2026-03-03 10:41:56 -0500

    Change the cron expression and update api key

--- lambdas/ingestion/index.js
+++ lambdas/ingestion/index.js
@@ -2,11 +2,8 @@ const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 const { DynamoDBDocumentClient, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
 const TABLE_NAME = process.env.TABLE_NAME;
-// const STOCK_API_KEY = process.env.STOCK_API_KEY;
-const STOCK_API_KEY = 'DywvFQC6wuHGutC_gfEgPwQhHTkp7xlp';
+const STOCK_API_KEY = process.env.STOCK_API_KEY;
 const stocks = ["AAPL", "MSFT", "GOOGL", "AMZN", "TSLA", "NVDA"];
-
-const savedResults = [{"status":"OK","from":"2026-02-26","symbol":"AAPL","open":274.945,"high":276.11,"low":270.795,"close":272.95,"volume":32345114.765942,"afterHours":271.7,"preMarket":273.46},{"status":"OK","from":"2026-02-26","symbol":"MSFT","open":404.71,"high":407.49,"low":398.74,"close":401.72,"volume":34405869.720193,"afterHours":397.193,"preMarket":400},{"status":"OK","from":"2026-02-26","symbol":"GOOGL","open":312.64,"high":313.14,"low":302.345,"close":307.38,"volume":36431231.916257,"afterHours":307.25,"preMarket":312.18},{"status":"OK","from":"2026-02-26","symbol":"AMZN","open":210.73,"high":211.05,"low":205.345,"close":207.92,"volume":47756765.609998,"afterHours":206.55,"preMarket":210},{"status":"OK","from":"2026-02-26","symbol":"TSLA","open":414.42,"high":416.81,"low":403.66,"close":408.58,"volume":53602497.658486,"afterHours":406.7,"preMarket":415.05},{"status":"OK","from":"2026-02-26","symbol":"NVDA","open":194.27,"high":194.29,"low":184.315,"close":184.89,"volume":360807907.423002,"afterHours":185,"preMarket":197.6}];
 
 exports.handler = async (event) => {
 
@@ -18,9 +15,10 @@ exports.handler = async (event) => {
             console.log("Weekend - No valid market data returned for this date.");
             return {statusCode: 200, body: "Weekend - no market data" };
         }
-        //const isoDate = new Date().toISOString().slice(0,10);
-        const isoDate = "2026-02-27"; // for testing. Free version doesn't allow today's data in real time
-        //console.log(isoDate);
+        // TODO: redeploy with new date
+        const isoDate = new Date(Date.now() - 86400000).toISOString().slice(0,10);
+        //const isoDate = "2026-02-26"; // for testing. Free version doesn't allow today's data in real time
+
 
         const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
         // fetches the open-close json for each of the stocks in batches due to free version having a 5 api call/min limit
@@ -72,7 +70,7 @@ exports.handler = async (event) => {
         });
         const direction = topMover.percentageChange > 0 ? "up" : "down";
 
-        // Build the DynamoDB item
+        //Build the DynamoDB item
         const item = {
             date: isoDate,
             symbol: topMover.symbol,
