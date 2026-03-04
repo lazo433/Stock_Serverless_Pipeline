df3aafaffab17afd36da3a26cc845ac6d0fc18cc
Henry <lazoo310@hotmail.com>
2026-03-04 13:46:51 -0500

    Add logic for retrieval function

--- lambdas/retrieval/index.js
+++ lambdas/retrieval/index.js
@@ -1,3 +1,50 @@
+const { DynamoDBClient, QueryCommand } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, ScanCommand} = require("@aws-sdk/lib-dynamodb");
+
+const TABLE_NAME = process.env.TABLE_NAME;
+
+const client  = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
 exports.handler = async (event) => {
-    // logic goes here
+    try {
+        const results = await docClient.send(
+            new QueryCommand({
+                TableName:TABLE_NAME,
+                KeyConditionExpression: "#pk = :pkval",
+                ExpressionAttributeNames: {
+                    "#pk" : "pk",
+                },
+                ExpressionAttributeValues: {
+                    ":pkval": "TOP_MOVERS",
+                },
+                ScanIndexForward: false,  // descending by data
+                Limit: 7, // grabs 7 records
+            })
+        );
+
+        const items = results.Items || [];
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({data: items}),
+        };
+    } catch (error) {
+        console.error("Retrieval error: ", error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                message: "Failed to retrieve top movers",
+                error: error.message,
+            }),
+        };
+    }
 };
